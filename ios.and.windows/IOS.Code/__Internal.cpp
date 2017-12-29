9ca21d62666de04a9b1350f0728d84bdfee94658
onemore <treert@sina.com>
2017-12-29 18:25:10 +0800

    。。。

---
+++ ios.and.windows/IOS.Code/__Internal.cpp
@@ -0,0 +1,16 @@
+﻿/*header
+    > File Name: __Internal.cpp
+    > Create Time: 2017-12-29 星期五 11时28分28秒
+    > Athor: treertzhu
+*/
+
+#include "__Internal.h"
+
+void U3D_RecvMsgFromUnity(const char * type , const char * content)
+{
+
+}
+const char * U3D_GetSDKConfig(const char * type, const char * content)
+{
+    
+}
